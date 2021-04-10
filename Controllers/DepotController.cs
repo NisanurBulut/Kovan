45fe1fba69e296c1590ac0ef73905e23bc6a1e39
NisanurBulut <nbulut@sarplojistik.com>
2021-04-10 19:04:35 +0300

    implemented controllers and managers

---
+++ Controllers/DepotController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kovan.Controllers
+{
+    public class DepotController : Controller
+    {
+        // GET: Depot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MaterialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kovan.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

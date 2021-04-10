ef55706b22b2d74c252cbf935adfa9c6351595fa
NisanurBulut <nbulut@sarplojistik.com>
2021-04-10 20:29:34 +0300

    implemented bootstrap 5

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,10 +20,9 @@ namespace Kovan
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                    ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }

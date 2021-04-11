877fbe843d8d9061e2ccc542afae395bec34675c
NisanurBulut <nbulut@sarplojistik.com>
2021-04-11 18:18:08 +0300

    implemented post shipments

--- Controllers/ShipmentController.cs
+++ Controllers/ShipmentController.cs
@@ -1,4 +1,5 @@
 ﻿using Kovan.Core.DataManager;
+using Kovan.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace Kovan.Controllers
                 ViewData["selectMaterials"] = dm.GetSelectMaterials();
             }
             return View();
+        }
+        [HttpPost]
+        public ActionResult SaveShipment(List<ShipmentModel> shipments)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/ShipmentModel.cs
+++ Models/ShipmentModel.cs
@@ -7,5 +7,11 @@ namespace Kovan.Models
 {
     public class ShipmentModel: BaseModel
     {
+        public int OriginDepotId { get; set; }
+        public int TargetDepotId { get; set; }
+        public int MaterialId { get; set; }
+        public int Amount { get; set; }
+        public DateTime ShipmentDate { get; set; } = DateTime.Now;
+      
     }
 }

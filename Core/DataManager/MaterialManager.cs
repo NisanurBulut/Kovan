6849a8249f801f8322366e6521227a98747c585a
NisanurBulut <nbulut@sarplojistik.com>
2021-04-11 12:47:53 +0300

    implemented  mapper for material

--- Core/DataManager/MaterialManager.cs
+++ Core/DataManager/MaterialManager.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Kovan.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Kovan.Core.DataManager
 {
-    public class MaterialManager
+    public class MaterialManager : DataAccessObject
     {
+        public List<MaterialModel> GetMaterials()
+        {
+            return dc.tDepots.Select(a => a.MaterialModel()).ToList();
+        }
     }
 }

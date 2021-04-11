31b21554916a4222df66f4d5f16879282a8e5c6c
NisanurBulut <nbulut@sarplojistik.com>
2021-04-11 13:53:12 +0300

    implemented shared manager

---
+++ Core/DataManager/SharedManager.cs
@@ -0,0 +1,21 @@
+﻿using Kovan.Core.Helpers;
+using Kovan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kovan.Core.DataManager
+{
+    public class SharedManager:DataAccessObject
+    {
+        public List<SelectModel> GetSelectDepots()
+        {
+            return dc.tDepots.Select(a => a.SelectModel()).ToList();
+        }
+        public List<SelectModel> GetSelectMaterials()
+        {
+            return dc.tMaterials.Select(a => a.SelectModel()).ToList();
+        }
+    }
+}

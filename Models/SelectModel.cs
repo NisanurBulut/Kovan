23fcda6738211e9fcf8a0ae7ed79f4a8feb40d9f
NisanurBulut <nbulut@sarplojistik.com>
2021-04-11 13:54:24 +0300

    created select model

--- Models/SelectModel.cs
+++ Models/SelectModel.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Kovan.Core.DataManager
+namespace Kovan.Models
 {
-    public class ShipmentMaterial : DataAccessObject
+    public class SelectModel:BaseModel
     {
+        public string Name { get; set; }
     }
 }

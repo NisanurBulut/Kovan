b2cd6d6bd6ca6ecd95c3751c43ccd4116031e358
NisanurBulut <nbulut@sarplojistik.com>
2021-04-11 12:45:00 +0300

    implemented material model

---
+++ Models/MaterialModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Kovan.Models
+{
+    public class MaterialModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Açıklama")]
+        public string Description { get; set; }
+        [DisplayName("Materyal Kodu")]
+
+        public string Code { get; set; }
+        [DisplayName("Materyal Birimi")]
+
+        public string Unit { get; set; }
+    }
+}
